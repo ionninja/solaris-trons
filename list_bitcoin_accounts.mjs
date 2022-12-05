3dd00058a4488a72c89e207d78bbe3623a5f2691
morph <hpromatem@protonmail.com>
2022-12-05 16:05:20 +0200

    list_bitcoin_accounts: вывод суммы балансов.

--- list_bitcoin_accounts.mjs
+++ list_bitcoin_accounts.mjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env tron
 
-// Скрипт возвращает карту аккаунтов и их балансов в YAML-формате.s
+// Скрипт возвращает карту аккаунтов и их балансов в YAML-формате.
 
 $.verbose = false;
 
@@ -8,6 +8,14 @@ let accounts = {};
 const listAccountsResult = await $`bitcoin-cli -conf=/share/bitcoin.conf listaccounts`;
 if (listAccountsResult.exitCode === 0) {
   accounts = JSON.parse(listAccountsResult.stdout);
+}
+
+if (argv.sum) {
+  let totalAmount = 0;
+  for (const amount of Object.values(accounts)) {
+    totalAmount += amount;
+  }
+  accounts['sum'] = totalAmount;
 }
 
 console.log(YAML.stringify(accounts));
