ad51bc33433fdc63a5efb8e2d78183d4d52f8c76
morph <hpromatem@protonmail.com>
2022-12-12 21:10:27 +0300

    + init_machine

---
+++ init_machine.mjs
@@ -0,0 +1,19 @@
+#!/usr/bin/env tron
+
+$.verbose = false;
+
+if (argv.skipUpgrade) {
+  await $`apt update`;
+  await $`apt -y upgrade`;
+}
+
+if (argv.host) {
+  const oldHostname = (await $`hostname`).trim();
+  await `hostname ${argv.hostname}`;
+  await $`sed -i 's/${oldHostname}/${argv.hostname}/g' /etc/hostname`;
+  await $`sed -i 's/${oldHostname}/${argv.hostname}/g' /etc/hosts`;
+  const newHostname = (await $`hostnamectl`).split('\n')[0].split(': ')[1].trim();
+  if (newHostname == argv.hostname) {
+    console.log(`hostname successfully changed to ${argv.hostname}`);
+  }
+}
