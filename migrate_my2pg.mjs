13dbe0bcd30f3069587d5d74ed387251ca0b985f
morph <hpromatem@protonmail.com>
2022-12-24 12:47:46 +0300

    migrate1to2: wip

--- migrate_my2pg.mjs
+++ migrate_my2pg.mjs
@@ -17,17 +17,41 @@ if (argv._.length === 0) {
   shops = argv._;
 }
 
+// Создаём базы данных
+
+
 for (const shop of shops) {
   cd(path.join(SOLARIS_PROJECTS_PATH, shop));
 
+  // Запускаем шоп
   let result = await $`./docker-compose.sh up -d`.nothrow();
   if (result.exitCode === 0) {
-    await runInternalScript("artisan.mjs", true, "down");
-    const contName = `${shop.toLowerCase()}_database_1`;
-    const contIp = await runInternalScript("docker_cont_ip.mjs", true, `${shop.toLowerCase()}_database_1`);
-    console.log(contName, contIp);
+    // Режим техобслуживания
+    const modeResult = await runInternalScript("artisan.mjs", true, "down");
+    if (!modeResult) {
+      console.error(`cannot maintenance ${shop} - ignore`);
+    }
+    const shopName = shop.toLowerCase();
+    const contName = `${shopName}_database_1`;
+    
+    // Узнаём IP нужного нам контейнера с mysql-ом.
+    const contIp = await runInternalScript("docker_cont_ip.mjs", true, contName);
+    // console.log(contName, contIp);
+
+    if (!contIp) {
+      console.error(`unknown ip address of '${contName}' docker container - ignore`)
+      continue;
+    }
+
+    // Запускаем pgloader.
+    const pgloaderResult = await $`pgloader mysql://root:rootpassword@${contIp}/database pgsql://solaris:solaris@10.20.30.1:5432/shop_${shopName}`;
+    if (pgloaderResult.exitCode === 0) {
+      console.error(pgloaderResult.stdout);
+    } else {
+      console.error(pgloaderResult.stderr);
+    }
   } else {
-    console.error(`${shop}: incomplete`);
+    console.error(result.stderr);
   }
 }
 
