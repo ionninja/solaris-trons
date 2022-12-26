6ffd0d0889f21c05530b0b43c144e5f1da1954ec
morph <hpromatem@protonmail.com>
2022-12-26 19:30:09 +0300

    start/stop shops

---
+++ start_shops.mjs
@@ -0,0 +1,31 @@
+\#!/usr/bin/env tron
+
+import { SOLARIS_PROJECTS_PATH, runInternalScript } from './common.mjs';
+
+$.verbose = false;
+
+const oldCwd = process.cwd();
+
+// Получаем список шопов на хосте.
+let shops;
+if (argv._.length === 0) {
+  shops = await runInternalScript("list_shops.mjs");
+  if (!shops) {
+    process.exit(1);
+  }
+} else {
+  shops = argv._;
+} 
+
+for (const shop of shops) {
+  cd(path.join(SOLARIS_PROJECTS_PATH, shop));
+
+  let result = await $`./docker-compose.sh up -d`.nothrow();
+  if (result.exitCode === 0) {
+    console.log(`${shop}: ok`);
+  } else {
+    console.error(`${shop}: incomplete`);
+  }
+}
+
+cd(oldCwd);
