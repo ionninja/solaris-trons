226e16d51687e7177c778df778473aeba3e863bb
morph <hpromatem@protonmail.com>
2022-12-27 12:55:14 +0300

    fix

--- solaris2_prepare_shops.mjs
+++ solaris2_prepare_shops.mjs
@@ -21,12 +21,10 @@ for (const shopId of shops) {
   cd(path.join(SOLARIS_PROJECTS_PATH, shopId));
 
   try {
-    // Стопаем шоп
     console.log(`${shopId}: stopping`);
     await runInternalScript("shop_down.mjs", true, shopId);
 
     console.log(`${shopId}: rm -R laravel_cache/*`);
-    // Удаляем кэш 
     try {
       await $`rm -R laravel_cache/*`;
     } catch (ex) {
@@ -35,34 +33,31 @@ for (const shopId of shops) {
     }
 
     console.log(`${shopId}: chown dirs`);
-    // Выставляем нужные права на папка
     await $`chown -R 1001:1001 database`;
     await $`chown -R 1000:33 storage`;
     await $`chown -R 1001:1001 cache`;
 
     console.log(`${shopId}: rebuild docker`);
-    // Пересобираем докер. 
     await runInternalScript("shop_up.mjs", true, shopId);
     
     console.log(`${shopId}: starting`);
-    // Запускаем докер
     await runInternalScript("shop_up.mjs", true, shopId);
     
     // artisan ...
     console.log(`${shopId}: artisan migrate --force`);
-    await runInternalScript("artisan.mjs", true, '--cmd', 'migrate --force');
-
+    await $`./docker-compose.sh exec php-fpm ./artisan migrate --force`;
+    
     console.log(`${shopId}: artisan mm2:close_preorders`);
-    await runInternalScript("artisan.mjs", true, '--cmd', 'mm2:close_preorders');
+    await $`./docker-compose.sh exec php-fpm ./artisan mm2:close_preorders`;
 
     console.log(`${shopId}: artisan cpp:init-system`);
-    await runInternalScript("artisan.mjs", true, '--cmd', 'cpp:init-system');
+    await $`./docker-compose.sh exec php-fpm ./artisan cpp:init-system`;
 
     console.log(`${shopId}: artisan route:clear`);
-    await runInternalScript("artisan.mjs", true, '--cmd', 'route:clear');
+    await $`./docker-compose.sh exec php-fpm ./artisan route:clear`;
 
     console.log(`${shopId}: artisan up`);
-    await runInternalScript("artisan.mjs", true, '--cmd', 'up');
+    await $`./docker-compose.sh exec php-fpm ./artisan up`;
   
   } catch (ex) {
     console.log(`${shopId}: fail`);
