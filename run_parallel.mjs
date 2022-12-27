4de72576f1c0ccbee4b415b4d296d6d13402ced8
morph <hpromatem@protonmail.com>
2022-12-27 18:06:36 +0300

    run_parallel: fix

--- run_parallel.mjs
+++ run_parallel.mjs
@@ -5,34 +5,17 @@ import { getShops, runInternalScript } from './common.mjs';
 $.verbose = true;
 
 const shops = await getShops();
-console.log(YAML.stringify(shops));
 const concurrent = argv.c;
-const shopsPerInstance = Math.floor(shops.length / concurrent);
 const queue = [];
 
-
-for (let i = 0; i <= concurrent; i++) {
-  const result = [];
-  for (let j = 0; j < shopsPerInstance; j++) {
-    const shopIndex = i * shopsPerInstance + j;
-    if (shopIndex >= shops.length) {
-      break;
-    }
-    result.push(shops[shopIndex]);
-  }
-  queue.push(result);
-}
-
-let done = false;
-
-const runParallel = () => {
+const runNext = () => {
   while (shops.length > 0) {
-    for (let i = 0; i < concurrent - queue.length; i++) {
+    for (let i = 0; i < concurrent - queue.length && shops.length > 0; i++) {
       const p = runInternalScript(argv.script, true, shops.shift());
-      p.then(() => runParallel()).catch(() => {});
+      p.then(() => runNext()).catch(() => {});
       queue.push(p);
     }
   }
 };
 
-runParallel();
+runNext();
