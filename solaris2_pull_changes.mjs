a51cfdfbf783535dc5aa792d6ca8793fd1208509
morph <hpromatem@protonmail.com>
2022-12-28 14:33:04 +0200

    solaris2_pull_changes

---
+++ solaris2_pull_changes.mjs
@@ -0,0 +1,31 @@
+#!/usr/bin/env tron
+
+import { runInternalScript } from './common.mjs';
+import { pull as gitPull } from "isomorphic-git";
+
+$.verbose = false;
+
+const oldCwd = process.cwd();
+
+let dir;
+switch (argv.t) {
+  case 'code': dir = '/share/app/code'; break;
+  case 'share': dir = '/share'; break;
+  case 'trons': dir = '/opt/solaris-trons'; break;
+}
+
+cd(dir);
+
+await gitPull({
+  fs,
+  http,
+  dir,
+  ref: 'master',
+  singleBranch: true,
+  username: 'morph',
+  token: 'glpat-pH8rxCGGi-pqfxMdsKqb',
+});
+
+await runInternalScript("solaris2_post_process.mjs");
+
+cd(oldCwd);
