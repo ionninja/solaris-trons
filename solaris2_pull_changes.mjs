c9df6d592a45dd2285cb357151b019c1d90e50f5
morph <hpromatem@protonmail.com>
2023-01-03 11:04:31 +0200

    pull_changes: update

--- solaris2_pull_changes.mjs
+++ solaris2_pull_changes.mjs
@@ -5,6 +5,7 @@
 import { pull as gitPull } from "isomorphic-git";
 import * as http from "isomorphic-git/http/node/index.cjs";
 import fs from "node:fs";
+import { argv } from "node:process";
 
 $.verbose = false;
 
@@ -53,6 +54,45 @@ if (argv.t !== 'trons' && !argv.skippp) {
     console.log("ok");
   } else {
     console.log(result.stderr.trim())
+  }
+} else if (argv.simplepp) {
+  // Назначаем каждому шопу свой redis database index
+  const shops = await getShops();
+
+  for (const shopId of shops) {
+    const shopBasePath = path.join(SOLARIS_PROJECTS_PATH, shopId);
+    cd(shopBasePath);
+
+    try {
+      console.log(`[${shopId}] stopping docker`);
+      await $`./docker-compose.sh down`;
+
+      console.log(`[${shopId}] rm -R laravel_cache/*`);
+      try {
+        await $`rm -R laravel_cache/*`;
+      } catch (ex) {
+        console.log(`[${shopId}] clear cache error`);
+        console.error(ex);
+      }
+
+      // ??? - оно может и не надо это тут, учитывая, что в `up` отдаётся флаг `--build`
+      console.log(`[${shopId}] rebuilding docker`);
+      await $`./docker-compose.sh build`;
+
+      console.log(`[${shopId}] starting docker with --build`);
+      await $`./docker-compose.sh up -d --build`;
+
+      console.log(`[${shopId}] artisan route:clear`);
+      await $`./docker-compose.sh exec php-fpm ./artisan route:clear`;
+
+      // artisan ...
+      console.log(`[${shopId}] artisan migrate --force`);
+      await $`./docker-compose.sh exec php-fpm ./artisan migrate --force`;
+
+    } catch (ex) {
+      console.log(`[${shopId}] fail`);
+      console.error(ex);
+    }
   }
 }
 
